154339ef42d87b7ba1e1701fb8c83b0112483622
selam123 <beletelakachew@gmail.com>
2022-09-05 23:56:00 -0700

    hg

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,3 +1,10 @@
 #!/usr/bin/node
 
-console.log(process.argv[2] + ' is ' + process.argv[3]);
+if (process.argv.length > 3) {
+  const array = process.argv.slice(2).map(Number);
+
+  array.splice(array.indexOf(Math.max.apply(null, array)), 1);
+  console.log(Math.max.apply(null, array));
+} else {
+  console.log(0);
+}
